43bdee2609f9199c8922c03ac99ebf4266180e98
nhoermann <hoermann@neuro.mpg.de>
2021-01-25 15:18:24 +0000

    Roman numbers converted up to number 8.

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ import pytest
 
 def from_roman(roman: str) -> int:
 #  length_roman = len(roman)
+  V_number = 0
+  I_number = 0
 
   if 'V' in roman:
     V_indices = roman.index('V')
 
     if V_indices == 1:
-      number = 4
+      V_number = 4
     elif V_indices == 0:
-      number = 5
+      V_number = 5
+      I_after_V = roman.count('I', V_indices)
+      I_number = I_after_V
   else:  
     I_count = roman.count('I')
-    number = I_count
-
-  return number
+    I_number = I_count
+    
+  number = V_number + I_number
+  return number 
 
 
 
@@ -28,7 +33,10 @@ cases = [
   ('II', 2),
   ('III', 3),
   ('IV', 4),
-  ('V', 5)
+  ('V', 5),
+  ('VI', 6),
+  ('VII', 7),
+  ('VIII', 8)
 ]
 @pytest.mark.parametrize(['num', 'roman'], cases)
 def test_roman_1(num: int, roman: str):
