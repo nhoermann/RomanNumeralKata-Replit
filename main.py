904e9951ec967d74481bc7b0d6dfc0508aff558a
nhoermann <hoermann@neuro.mpg.de>
2021-01-25 15:04:25 +0000

    Converting the roman numbers up to number 5.

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import pytest
 # Source Code:
 
 def from_roman(roman: str) -> int:
+#  length_roman = len(roman)
+
   if 'V' in roman:
     V_indices = roman.index('V')
 
     if V_indices == 1:
       number = 4
+    elif V_indices == 0:
+      number = 5
   else:  
     I_count = roman.count('I')
     number = I_count
@@ -24,6 +28,7 @@ cases = [
   ('II', 2),
   ('III', 3),
   ('IV', 4),
+  ('V', 5)
 ]
 @pytest.mark.parametrize(['num', 'roman'], cases)
 def test_roman_1(num: int, roman: str):
